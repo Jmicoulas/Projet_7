cac01920943b1396d3508b0854202b60ddfd1d95
Jonathan Micoulas <j.micoulas@laposte.net>
2021-03-24 11:48:35 +0100

    + dossier + npm init

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "GroupomaniaDB",
+    password: "Praline1306*",
+    database: "groupomania"
+    });
+
+db.connect(function(err) {
+        if (err) throw err;
+        console.log("Connecté à la base de données MySQL!");
+      });
+
+module.exports = app;
